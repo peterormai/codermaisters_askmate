28eaaa762a56f55ac13990714f31bcf4436405b5
SZkristof <szabokristof0@gmail.com>
2017-05-10 13:54:25 +0200

    Question time, ID, 0 values added

--- new_question.py
+++ new_question.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect
+import time
 
 app = Flask(__name__)
 
@@ -8,23 +9,38 @@ def question():
     return render_template("new_question.html")
 
 
+def count_lines():
+    with open("new_questions.csv") as file:
+        for i, line in enumerate(file):
+            pass
+
+
 @app.route("/new_question", methods=["POST"])
 def submit_new_question():
-    new_question = request.form["new_question"]
-    new_question_long = request.form["new_question_long"]
+    new_question_title = request.form["new_question"]
+    new_question_message = request.form["new_question_long"]
+    count_view = 0
+    count_like = 0
+    question_time = time.time()
 
-    question_id = 0
     with open("new_questions.csv", "r") as file:
-        for i in file:
-            if i == "\n":
-                question_id = int(question_id) + 1
+        data_list = file.read().splitlines()
+        data_list = [item.split(",") for item in data_list]
+        if len(data_list) > 0:
+            question_id = str(int(data_list[-1][0]) + 1)
+        else:
+            question_id = 0
 
     with open("new_questions.csv", "a") as file:
-        file.write(str(question_id + "\t"))
-        file.write(str(new_question + "\t"))
-        file.write(str(new_question_long + "\n"))
+        file.write(str(question_id) + ",")
+        file.write(str(question_time) + ",")
+        file.write(str(count_view) + ",")
+        file.write(str(count_like) + ",")
+        file.write(str(new_question_title + ","))
+        file.write(str(new_question_message + "\n"))
 
     return render_template("new_question.html")
+
 
 if __name__ == "__main__":
     app.run()
