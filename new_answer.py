1e2ecd15a6960389af2fcabf3825ae677a1f00fe
helgaKalicz <kalicz.helga@gmail.com>
2017-05-10 21:05:13 +0200

    Encode, decode prototype

--- new_answer.py
+++ new_answer.py
@@ -1,16 +1,32 @@
 from flask import Flask, render_template, request, redirect
 import time
-# Nézd át az importot
+import base64
 app = Flask(__name__)
+
+
+def list_of_file(filename):
+    with open(filename) as file:
+        filelist = [line.split(',') for line in file.read().splitlines()]
+    if filename == 'answer.csv':
+        for lines in filelist:
+            lines[1] = time.ctime(int(lines[1]))
+            lines[4] = base64.b64decode(str.encode(lines[4])).decode()
+    elif filename == 'question.csv':
+        for lines in filelist:
+            lines[1] = time.ctime(int(lines[1]))
+            lines[4] = base64.b64decode(str.encode(lines[4])).decode()
+            lines[5] = base64.b64decode(str.encode(lines[5])).decode()
+    else:
+        pass
+    return filelist
 
 
 @app.route('/question/<int:question_id>/new_answer')
 def new_answer(question_id):
-    with open('question.csv') as file:
-        question_list = [question.split(',') for question in file.read().splitlines()]
+    question_list = list_of_file('question.csv')
     selected_question = ''
     for question in question_list:
-        if int(question[0]) == int(question_id):
+        if int(question[0]) == question_id:
             selected_question = question
             break
     webpage_title = 'Post an Answer'
@@ -21,18 +37,15 @@ def new_answer(question_id):
 
 @app.route('/question/<int:question_id>/new_answer', methods=['POST'])
 def add_answer(question_id):
-    with open('answer.csv') as file:
-        answer_id = int([item.split(',') for item in file.read().splitlines()][-1][0]) + 1 #Ne így legyen
-    message = request.form['answer']
-    image = ''
+    answer_id = list_of_file('answer.csv')
     with open('answer.csv', 'a') as file:
-        file.write(str(answer_id) + ',')
-        file.write(str(time.time()) + ',')
+        file.write(str(int(answer_id[-1][0]) + 1) + ',')
+        file.write(str(int(time.time())) + ',')
         file.write(str(0) + ',')    # Vote number
         file.write(str(question_id) + ',')
-        file.write(str(message) + ',')  # Át akarom írni!!
-        file.write(str(image) + '\n')
-    return redirect('/')
+        file.write(str(base64.b64encode(str.encode(request.form['answer']).decode())) + ',')
+        file.write(str('') + '\n')  # This will be the image
+    return redirect('/list')
 
 
 def main():
