970e099ecc92cb44870726c0c4ea4395038ea12f
peterormai <ormaipeter.fx@gmail.com>
2017-05-10 16:12:43 +0200

    html update for common css file

--- display_question.py
+++ display_question.py
@@ -8,25 +8,25 @@ app = Flask(__name__)
 @app.route('/list', methods=['GET'])
 @app.route('/', methods=['GET'])
 def main_page():
-    return index.list_page()
+    return index.list_questions()
 
 
-@app.route('/question/<int:id>')
-def question_display(id):
+@app.route('/question/<int:question_id>')
+def question_display(question_id):
     webpage_title = 'Question & answers'
     with open('question.csv') as question:
         question_list = question.read().splitlines()
         question_list = [item.split(",") for item in question_list]
         selected_question = []
         for item in question_list:
-            if int(item[0]) == int(id):
+            if int(item[0]) == int(question_id):
                 selected_question = item
     with open('answer.csv') as answer:
         answer_list = answer.read().splitlines()
         answer_list = [item.split(",") for item in answer_list]
         related_answers = []
         for item in answer_list:
-            if int(item[3]) == int(id):
+            if int(item[3]) == int(question_id):
                 related_answers.append(item)
         return render_template('question_display.html',
                                question=selected_question,
@@ -34,14 +34,14 @@ def question_display(id):
                                webpage_title=webpage_title)
 
 
-@app.route('/answer/<int:id>/delete', methods=['POST'])
-def delete_answer(id):
+@app.route('/answer/<int:answer_id>/delete', methods=['POST'])
+def delete_answer(answer_id):
     question_id = 0
     with open('answer.csv') as answer:
         answer_list = answer.read().splitlines()
         answer_list = [item.split(",") for item in answer_list]
         for item in answer_list:
-            if int(item[0]) == int(id):
+            if int(item[0]) == int(answer_id):
                 answer_list.remove(item)
                 question_id = item[3]
     with open('answer.csv', 'w') as file:
@@ -85,6 +85,12 @@ def main():
 if __name__ == '__main__':
     main()
 
-
+# GIT!!!!!!!!
+# unix base 64?
 # több functionbe?
 # kérdés url nem szűnik meg
+# követelményeket megnézni, feladatot újraolvasni, git!!!!!
+# HOLNAP: szebb html megjelenítés, clean code, követelményeknek megfelelő változtatások
+# hibakezelés, 404, vessző, enter
+# answe_id jav
+# id int??
