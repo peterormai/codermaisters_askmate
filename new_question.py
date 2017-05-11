9b847d2bd396544a3691b91a2be0667219c134dd
SZkristof <szabokristof0@gmail.com>
2017-05-11 13:28:24 +0200

    Encoding in seperate function, usability for others

--- new_question.py
+++ new_question.py
@@ -10,18 +10,21 @@ def question():
     return render_template("new_question.html")
 
 
+def encode_text(plain_text):
+    encoded_text = base64.urlsafe_b64encode(plain_text.encode())
+    return encoded_text
+
+
 @app.route("/new_question", methods=["POST"])
 def submit_new_question():
     new_question_title = request.form["new_question"]
-    new_question_title_encoded = base64.b64encode(new_question_title.encode())
+    new_question_title_encoded = encode_text(new_question_title)
     new_question_message = request.form["new_question_long"]
-    new_question_message_encoded = base64.b64encode(new_question_message.encode())
+    new_question_message_encoded = encode_text(new_question_message)
     picture_url = request.form["picture"]
     count_view = 0
     count_like = 0
     question_time = time.time()
-    new_question_title_decoded = base64.b64encode(new_question_title_encoded)
-    print(new_question_title_decoded)
 
     with open("new_questions.csv", "r") as file:
         data_list = file.read().splitlines()
@@ -36,11 +39,14 @@ def submit_new_question():
         file.write(str(question_time) + ",")
         file.write(str(count_view) + ",")
         file.write(str(count_like) + ",")
-        file.write(str(new_question_title_encoded) + ",")
-        file.write(str(new_question_message_encoded) + ",")
+        file.write(str(new_question_title_encoded)[2:-1] + ",")
+        file.write(str(new_question_message_encoded)[2:-1] + ",")
         file.write(str(picture_url + "\n"))
 
-    print(new_question_title_decoded)
+    with open("new_questions.csv", "r") as file:
+        data_list = file.read().splitlines()
+        data_list = [item.split(",") for item in data_list]
+
     return render_template("new_question.html")
 
     # redirect to Peti's page at the end with ...question/current_id !!!!!! ****
