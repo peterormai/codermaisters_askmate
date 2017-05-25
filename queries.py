57612bcfb4f497a6e9b42defe7af91fd2474c42a
barnabastoth <barnabastoth94@gmail.com>
2017-05-25 12:13:41 +0200

    refactoring the queries

--- queries.py
+++ queries.py
@@ -2,6 +2,9 @@ import psycopg2
 
 
 def user_datas():
+    """Read the nessecery information from the user_file to
+    connect to the database, such as dbname, username, password
+    """
     with open('user.txt') as file:
         data = file.read()
         data = data.split(',')
@@ -9,6 +12,9 @@ def user_datas():
 
 
 def fetch_database(query):
+    """Connects to the database to retrieve data, then
+    returns it.
+    """
     try:
         data = user_datas()
         connect_str = "dbname={0} user={0} host='localhost' password={1}".format(data[0], data[1])
@@ -28,6 +34,9 @@ def fetch_database(query):
 
 
 def modify_database(query):
+    """Connects to the database then modifies the data
+    without fetching anything.
+    """
     try:
         data = user_datas()
         connect_str = "dbname={0} user={0} host='localhost' password={1}".format(data[0], data[1])
@@ -44,82 +53,84 @@ def modify_database(query):
     #         connection.close()
 
 
-def all_questions():
-    result = fetch_database("""SELECT * FROM question ORDER BY id;""")
-    return result
+def get_all_questions():
+    """Returns all the questions"""
+    return fetch_database("""SELECT * FROM question ORDER BY id;""")
 
 
-def show_question(id):
-    result = fetch_database("""SELECT title FROM question WHERE id={};""".format(id))
-    return result
+def edit_question(id):
+    """Returns the currect question details that are to be changed by user"""
+    return fetch_database("""SELECT title FROM question WHERE id={};""".format(id))
 
 
-def update_question(title, id):
-    result = modify_database("""UPDATE question SET title='{}' WHERE id={};""".format(title, id))
+def update_question_query(title, message, id):
+    """Updates the database with the edited question details"""
+    return modify_database("""UPDATE question SET title='{}', message='{}' WHERE id={};""".format(title, message, id))
 
 
-def display_question(id):
-    result = fetch_database("""SELECT * FROM question WHERE id={}""".format(id))
-    return result
+def get_question_details(id):
+    """Returns all the details about a specific question"""
+    return fetch_database("""SELECT * FROM question WHERE id={}""".format(id))
 
 
-def display_answer(id):
-    result = fetch_database("""SELECT * FROM answer WHERE question_id={} ORDER BY id""".format(id))
-    return result
+def get_question_answers(id):
+    """Returns all the answers to a specific question"""
+    return fetch_database("""SELECT * FROM answer WHERE question_id={} ORDER BY id""".format(id))
 
 
-def display_question_comment(id):
-    result = fetch_database("""SELECT message FROM comment WHERE question_id={}""".format(id))
-    return result
+def get_question_comments(id):
+    """Returns all the comments to a specific question"""
+    return fetch_database("""SELECT message FROM comment WHERE question_id = {}""".format(id))
 
 
-def display_answer_comment(answer_id):
-    result = fetch_database(
-        """SELECT answer_id, message, submission_time FROM comment WHERE answer_id={}""".format(answer_id))
-    return result
+def get_answer_comment(answer_id):
+    """Returns all the comments to a specific question-answer"""
+    return fetch_database("""SELECT answer_id, message, submission_time
+                         FROM comment WHERE answer_id = {}""".format(answer_id))
 
 
-def answer_comment_ids(id):
-    answer_ids = fetch_database("""SELECT id FROM answer WHERE question_id={}""".format(id))
+def get_answer_comment_ids(id):
+    """Returns all the answer_comments IDs"""
+    answer_ids = fetch_database("""SELECT id FROM answer WHERE question_id = {}""".format(id))
     id_numbers = []
     for item in answer_ids:
         id_numbers.append("".join(map(str, item)))
     return id_numbers
 
 
-def delete_all_answer(id):
-    result = modify_database("""DELETE FROM answer WHERE question_id={};""".format(id))
+# def delete_all_answer(id):
+#     result = modify_database("""DELETE FROM answer WHERE question_id = {}; """.format(id))
 
 
-def delete_question_comment(question_id):
-    result = modify_database("""DELETE FROM comment WHERE question_id={};""".format(question_id))
+# def delete_question_comment(question_id):
+#     return modify_database("""DELETE FROM comment WHERE question_id = {}; """.format(question_id))
 
 
 def delete_one_answer(answer_id):
-    result = modify_database("""DELETE FROM answer WHERE id={};""".format(answer_id))
+    return modify_database("""DELETE FROM answer WHERE id = {}; """.format(answer_id))
 
 
 def delete_answer_comment(answer_id):
-    result = modify_database("""DELETE FROM comment WHERE answer_id={};""".format(answer_id))
+    result = modify_database("""DELETE FROM comment WHERE answer_id = {}; """.format(answer_id))
 
 
 def delete_question(id):
-    result = modify_database("""DELETE FROM question WHERE id={};""".format(id))
+    result = modify_database("""DELETE FROM question WHERE id = {}; """.format(id))
 
 
 def add_new_answer(submission_time, vote_number, question_id, message, image):
     result = modify_database("""INSERT INTO answer(submission_time, vote_number, question_id, message, image) SELECT
-                                '{}', {}, {}, '{}', '{}';""".format(submission_time, vote_number, question_id, message, image))
+                                '{}', {}, {}, '{}', '{}'; """.format(submission_time, vote_number, question_id, message, image))
 
 
 def handle_question_like(id, like_value):
     result = modify_database(
-        """UPDATE question SET vote_number = vote_number + {} WHERE id={}""".format(like_value, id))
+        """UPDATE question SET vote_number = vote_number + {} WHERE id = {}""".format(like_value, id))
 
 
 def handle_answer_like(id, like_value):
     result = modify_database(
-        """UPDATE answer SET vote_number = vote_number + {} WHERE id={}""".format(like_value, id))
+        """UPDATE answer SET vote_number = vote_number + {} WHERE id = {}""".format(like_value, id))
 
 
 def submit_new_question(submission_time, view_number, vote_number, title, message, image):
